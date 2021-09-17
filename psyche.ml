666aaf31965af4bc86fd891df1adfdce71a9adf4
0918nobita <nobita.0918@gmail.com>
2021-09-17 19:22:05 +0900

    lwt を用いたサンプルを追加した

--- psyche.ml
+++ psyche.ml
@@ -1,1 +1,15 @@
-let () = print_endline "Hello, World!"
+open Lwt.Syntax
+
+let () =
+  let sleepA =
+    let* () = Lwt_unix.sleep 1. in
+    print_endline "A";
+    Lwt.return ()
+  in
+  let sleepB =
+    let* () = Lwt_unix.sleep 3. in
+    print_endline "B";
+    Lwt.return ()
+  in
+  let _ = Lwt_main.run @@ Lwt.all [sleepA; sleepB] in
+  print_endline "C"
