6ad49360be179963333e2f70900efadb99823816
0918nobita <nobita.0918@gmail.com>
2021-09-22 01:06:11 +0900

    Remove debug code

--- compiler/compiler.ml
+++ compiler/compiler.ml
@@ -1,20 +1,2 @@
-open Base
-
-let (%>) f g x = g (f x)
-
-let make_decoder () =
-  let nln = `Readline (Uchar.of_scalar_exn 0x000A) in
-  let encoding = `UTF_8 in
-  let src = `String "あいうえお" in
-  Uutf.decoder ~nln ~encoding src
-
-let print_picked_uchar decoder =
-  Stdlib.Format.printf "%a\n" Uutf.pp_decode @@ Uutf.decode decoder
-
 let () =
-  let decoder = make_decoder () in
-  print_picked_uchar decoder;
-  print_picked_uchar decoder;
-  print_picked_uchar decoder;
-  print_picked_uchar decoder;
-  print_picked_uchar decoder
+  Stdlib.print_endline "Psyche compiler"
