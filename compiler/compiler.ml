0dd7775c10cd601a216f942402e43834768ad30c
0918nobita <nobita.0918@gmail.com>
2021-09-21 16:31:16 +0900

    Do refactor

--- compiler/compiler.ml
+++ compiler/compiler.ml
@@ -1,30 +1,20 @@
 open Base
 
+let (%>) f g x = g (f x)
+
 let make_decoder () =
   let nln = `Readline (Uchar.of_scalar_exn 0x000A) in
   let encoding = `UTF_8 in
   let src = `String "あいうえお" in
   Uutf.decoder ~nln ~encoding src
 
-let pick_uchar decoder =
-  match Uutf.decode decoder with
-  | `Uchar u -> Some u
-  | _ -> None
-
-let print_uchar_opt =
-  function
-  | Some n ->
-    n
-    |> Uchar.to_scalar
-    |> Printf.sprintf "Some %d"
-    |> Stdlib.print_endline
-  | None ->
-    Stdlib.print_endline "None"
+let print_picked_uchar decoder =
+  Stdlib.Format.printf "%a\n" Uutf.pp_decode @@ Uutf.decode decoder
 
 let () =
   let decoder = make_decoder () in
-  print_uchar_opt @@ pick_uchar decoder;
-  print_uchar_opt @@ pick_uchar decoder;
-  print_uchar_opt @@ pick_uchar decoder;
-  print_uchar_opt @@ pick_uchar decoder;
-  print_uchar_opt @@ pick_uchar decoder
+  print_picked_uchar decoder;
+  print_picked_uchar decoder;
+  print_picked_uchar decoder;
+  print_picked_uchar decoder;
+  print_picked_uchar decoder
