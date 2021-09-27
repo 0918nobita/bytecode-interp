406628774dd61300d048bee7ee2bea203d2c8ee3
0918nobita <nobita.0918@gmail.com>
2021-09-27 16:11:12 +0900

    Define type-level natural number and list including a type parameter which represents its length

--- unicode/unicode.ml
+++ unicode/unicode.ml
@@ -1,8 +1,58 @@
 open Base
 
-type t = Uchar.t list
+module Nat = struct
+  type z
+
+  type 'a s
+end
+
+module TList : sig
+  type ('len, 'elem) t
+
+  val _nil : (Nat.z, 'elem) t
+
+  val _cons : 'elem -> ('n, 'elem) t -> ('n Nat.s, 'elem) t
+
+  val _hd : ('n Nat.s, 'elem) t -> 'elem
+
+  val _tl : ('n Nat.s, 'elem) t -> ('n, 'elem) t
+end = struct
+  type ('len, 'elem) t = 'elem list
+
+  let _nil = []
+
+  let _cons x xs = x :: xs
+
+  let _hd = List.hd_exn
+
+  let _tl = List.tl_exn
+end
+
+type bit = bool
+
+type _0 = Nat.z
+
+type _1 = _0 Nat.s
+
+type _2 = _1 Nat.s
+
+type _3 = _2 Nat.s
+
+type _4 = _3 Nat.s
+
+type _5 = _4 Nat.s
+
+type _6 = _5 Nat.s
+
+type _7 = _6 Nat.s
+
+type _8 = _7 Nat.s
+
+type _byte = (_8, bit) TList.t
 
 let empty = []
+
+type t = Uchar.t list
 
 let of_string str : t =
   let decoder =
