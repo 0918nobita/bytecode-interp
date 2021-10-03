20ff9d70713f8ecdeab64f41af6af45958910a80
0918nobita <nobita.0918@gmail.com>
2021-10-03 21:39:48 +0900

    [skip ci] code_point 型を定義した

--- unicode/unicode.v
+++ unicode/unicode.v
@@ -1,7 +1,31 @@
-Require Import Ascii.
+Require Import Ascii String.
 Require Extraction.
+Import Hexadecimal.
 
-Definition u8 := ascii.
+Local Definition code_point_upper_bound :=
+  Nat.of_num_uint
+    (Number.UIntHexadecimal
+      (D1 (D0 (Df (Df (Df (Df Nil))))))).
 
-Extract Inductive bool => "bool" [ "true" "false" ].
-Extraction "unicode.ml" u8.
+Definition code_point := { n : nat | n <= code_point_upper_bound }.
+
+Lemma two_le_upper_bound : 2 <= code_point_upper_bound.
+Proof.
+  apply le_n_S.
+  apply le_n_S.
+  apply le_0_n.
+Qed.
+
+Definition code_point_example :=
+  exist (fun x => x <= code_point_upper_bound) 2 two_le_upper_bound.
+
+Extract Inductive bool =>
+  "bool"
+  [ "true" "false" ].
+
+Extract Inductive nat =>
+  "int"
+  [ "0" "succ" ]
+  "(fun f0 fS -> function 0 -> f0 () | n -> fS (pred n))".
+
+Extraction "unicode.ml" code_point.
