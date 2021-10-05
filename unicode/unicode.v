facfaec6655f0478ff23c8ba2a906d781409e984
0918nobita <nobita.0918@gmail.com>
2021-10-06 02:59:56 +0900

    [skip ci] モーダスポネンス、ヒルベルトの公理Sを形式化した

--- unicode/unicode.v
+++ unicode/unicode.v
@@ -1,3 +1,37 @@
+From mathcomp Require Import ssreflect.
+
+Section ModusPonens.
+  Variables X Y : Prop.
+
+  Hypothesis XtoY_is_true : X -> Y.
+  Hypothesis X_is_true : X.
+
+  Theorem MP : Y.
+  Proof.
+    move: X_is_true.
+    by [].
+  Qed.
+End ModusPonens.
+
+Section HilbertSAxiom.
+  Variables A B C : Prop.
+
+  Theorem HS1 : (A -> (B -> C)) -> ((A -> B) -> (A -> C)).
+  Proof.
+    move=> AtoBtoC_is_true.
+    move=> AtoB_is_true.
+    move=> A_is_true.
+    apply: (MP B C).
+    apply: (MP A (B -> C)).
+    by [].
+    by [].
+    apply: (MP A B).
+    by [].
+    by [].
+  Qed.
+End HilbertSAxiom.
+
+(*
 Require Import Ascii String NArith.
 Require Extraction.
 Open Scope N_scope.
@@ -29,3 +63,4 @@ Extract Inductive nat =>
   "(fun f0 fS -> function 0 -> f0 () | n -> fS (pred n))".
 
 Extraction "unicode.ml" code_point.
+*)
