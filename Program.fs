952b0f89d359408df43a9d7fec2c9d4ed8f5e14b
0918nobita <nobita.0918@gmail.com>
2022-06-16 08:18:09 +0900

    :label: Expr, Stmt, Program 型を追加した

--- Program.fs
+++ Program.fs
@@ -1,48 +1,32 @@
 module Program
 
-// 全く F# らしさのないコードだが、「ふつうのコンパイラをつくろう」で扱っている
-// C♭言語コンパイラの実装をそっくり移植して正常に動作させることを当面の目標としているので許してほしい。
-// モジュール単位で、移植作業の終わったものから F# らしいコードにリファクタリングする予定である。
+type Expr =
+    | IntLit of int
 
-// open System.IO
+    override this.ToString() =
+        match this with
+        | IntLit x -> string x
 
-exception OptionParseException of msg : string
+type Stmt =
+    | Debug of Expr
 
-type Options () =
-    let mutable _sourceFiles = []
-    let mutable _ldArgs = []
+    override this.ToString() =
+        match this with
+        | Debug expr -> $"debug %O{expr}"
 
-    static member Parse argv =
-        let opts = Options()
-        opts.ParseArgv argv
-        opts
+type Program =
+    | Program of Stmt list
 
-    member _.ParseArgv _origArgv =
-        // TODO: 移植
-        ()
+    static member Unwrap((Program stmts)) = stmts
 
-    // member _.PrinUsage (writer : TextWriter) =
-        // TODO: 移植
-        // writer.WriteLine "Usage: cbc [options] file..."
-
-type Compiler (_programName : string) =
-    // TODO: errorHandler の初期化
-    member this.CommandMain argv =
-        // TODO: 移植
-        let _opts = this.ParseOptions();
-        ()
-
-    member _.ParseOptions argv =
-        try
-            Options.Parse argv
-        with
-        | OptionParseException _msg ->
-            // TODO: 移植
-            exit 1
+    override this.ToString() =
+        this
+        |> Program.Unwrap
+        |> List.map string
+        |> String.concat "\n"
 
 [<EntryPoint>]
 let main argv =
-    let programName = "cbc"
-    let _version = "1.0.0"
-    Compiler(programName).CommandMain argv
+    printfn "Psyche Compiler"
+    printfn "%O" <| Program [ Debug(IntLit 42) ]
     0
