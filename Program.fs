1e149a35213350f2435bb42d1acaa87242743ec1
0918nobita <nobita.0918@gmail.com>
2022-06-16 09:25:14 +0900

    :sparkles: ECMAScript の AST を定義し始めた

--- Program.fs
+++ Program.fs
@@ -1,5 +1,37 @@
 module Program
 
+open Thoth.Json.Net
+
+module EcmaScript =
+    type IJsonSerializable =
+        abstract member ToJson: unit -> JsonValue
+
+    type Statement =
+        | ExpressionStatement
+        // TODO: Add more variants
+
+        interface IJsonSerializable with
+            member this.ToJson() =
+                match this with
+                | ExpressionStatement -> Encode.object [ "type", Encode.string "ExpressionStatement" ]
+
+    type Program =
+        | Program of {| Body: Statement array |}
+
+        static member Unwrap((Program program)) = program.Body
+
+        interface IJsonSerializable with
+            member this.ToJson() =
+                let body = Program.Unwrap this
+
+                let bodyJson =
+                    body
+                    |> Array.map (fun stmt -> (stmt :> IJsonSerializable).ToJson())
+                    |> Encode.array
+
+                Encode.object [ "type", Encode.string "Program"
+                                "body", bodyJson ]
+
 type Expr =
     | IntLit of int
 
@@ -29,4 +61,12 @@ type Program =
 let main argv =
     printfn "Psyche Compiler"
     printfn "%O" <| Program [ Debug(IntLit 42) ]
+
+    let ecmaScriptAst =
+        EcmaScript.Program {| Body = [| EcmaScript.ExpressionStatement |] |}
+
+    (ecmaScriptAst :> EcmaScript.IJsonSerializable)
+        .ToJson()
+    |> printfn "%O"
+
     0
