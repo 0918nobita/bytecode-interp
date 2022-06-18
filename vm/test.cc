2440eede2d1c1b36731438d8617285613d05a2a1
0918nobita <nobita.0918@gmail.com>
2022-06-18 21:14:44 +0900

    :white_check_mark: chunk に関するテストを追加した

--- vm/test.cc
+++ vm/test.cc
@@ -1,11 +1,35 @@
 #include <CppUTest/CommandLineTestRunner.h>
 
-TEST_GROUP(Add) {};
+extern "C" {
+#include "chunk.h"
+}
 
-TEST(Add, FirstTest) {
-    int actual = 3 + 4;
-    int expected = 7;
-    CHECK_EQUAL(expected, actual);
+TEST_GROUP(Chunk) {};
+
+TEST(Chunk, GrowAndShrink) {
+    Chunk chunk;
+    initChunk(&chunk);
+    CHECK_EQUAL(0, chunk.count);
+    CHECK_EQUAL(0, chunk.capacity);
+    CHECK_EQUAL(NULL, chunk.code);
+
+    writeChunk(&chunk, 12);
+    CHECK_EQUAL(1, chunk.count);
+    CHECK_EQUAL(8, chunk.capacity);
+    CHECK_EQUAL(12, chunk.code[0]);
+
+    for (int i = 1; i <= 7; i++) writeChunk(&chunk, 24);
+    CHECK_EQUAL(8, chunk.count);
+    CHECK_EQUAL(8, chunk.capacity);
+
+    writeChunk(&chunk, 48);
+    CHECK_EQUAL(9, chunk.count);
+    CHECK_EQUAL(16, chunk.capacity);
+
+    freeChunk(&chunk);
+    CHECK_EQUAL(0, chunk.count);
+    CHECK_EQUAL(0, chunk.capacity);
+    CHECK_EQUAL(NULL, chunk.code);
 }
 
 int main(int argc, char* argv[]) {
